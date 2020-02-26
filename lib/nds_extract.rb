07503c0c4ffc8b9dd102a65f95091ac8f40d73b6
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-26 18:42:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,7 +34,7 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  movi
+  
   
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
