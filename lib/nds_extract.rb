a05db6296bd6cf0b5c5fcb061d7df30be57654bc
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-26 19:07:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -75,6 +75,7 @@ def gross_per_studio(collection)
 end
 
 def movies_with_directors_set(source)
+  output = []
   
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
