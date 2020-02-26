18fc227e86dbb2b0ec5777f00add53c03c3009fc
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-26 19:15:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -85,16 +85,6 @@ def movies_with_directors_set(source)
     i += 1
   end
   output
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
 end
 
 # ----------------    End of Your Code Region --------------------
