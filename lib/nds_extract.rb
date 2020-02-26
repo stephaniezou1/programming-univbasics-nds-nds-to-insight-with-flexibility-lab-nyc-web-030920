cd5f788f5ea6b266f489c16f670ece1521c522c8
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-26 19:26:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -75,6 +75,7 @@ def gross_per_studio(collection)
     
     i += 1
   end
+  output
   
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
