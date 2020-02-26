6cfb836eea5a39aec41d0cd7b9631fb890c39b6b
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-26 19:10:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -82,6 +82,7 @@ def movies_with_directors_set(source)
     director_names = director_information_hash[:name]
     director_movies = director_information_hash[:movies]
     output << movies_with_directors_key(name, movies_collection)
+    
   end
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
